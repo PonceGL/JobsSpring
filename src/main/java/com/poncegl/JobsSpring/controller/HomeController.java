d571d3038e24becfa76a0705ca9c3ecda581e805
“PonceGL” <poncianogl@hotmail.com>
2025-10-28 17:21:40 -0600

    feat(home): Update home page to display dynamic message and current date

--- src/main/java/com/poncegl/JobsSpring/controller/HomeController.java
+++ src/main/java/com/poncegl/JobsSpring/controller/HomeController.java
@@ -1,13 +1,18 @@
 package com.poncegl.JobsSpring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Date;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("message", "Empleos App");
+        model.addAttribute("date", new Date());
         return "home";
     }
 }
