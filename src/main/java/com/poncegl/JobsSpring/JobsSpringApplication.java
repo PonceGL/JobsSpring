699750354d377b754b3659f669c9feaf47188f94
“PonceGL” <poncianogl@hotmail.com>
2025-10-28 16:43:09 -0600

    Initial commit

---
+++ src/main/java/com/poncegl/JobsSpring/JobsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.poncegl.JobsSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobsSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poncegl/JobsSpring/JobsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poncegl.JobsSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
